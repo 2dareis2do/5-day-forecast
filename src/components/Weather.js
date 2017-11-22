06c0471537ab6e10037618a62c36620c141d7df8
dan <info@danlobo.co.uk>
2017-11-22 03:16:32 +0000

    stable

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,6 @@
-// import axios from 'axios';
 import React from 'react';
 import WeatherStore from '../stores/WeatherStore.js';
 import { getForecast } from '../actions/WeatherServerActions.js';
-import { getForecast2 } from '../actions/WeatherServerActions.js';
-import { getForecastResponse } from '../actions/WeatherServerActions.js';
 
 const itemStyle = {
   display: 'inline-block',
@@ -22,27 +19,21 @@ const ulStyle = {
   margin: 0
 }
 
-// function checkStatus(response) {
-//   if (response.status >= 200 && response.status < 300) {
-//     return response
-//   } else {
-//     var error = new Error(response.statusText)
-//     error.response = response
-//     throw error
-//   }
-// }
-
-// function parseJSON(response) {
-//   return response.data;
-// }
+const divStyle = {
+  height: '100px',
+  width: '20%',
+  textAlign: 'center',
+  padding: '1em',
+  margin: '0 auto 1em',
+  display: 'inline-block',
+  verticalAlign: 'top'
+}
 
 export default class Weather extends React.Component {
 
   constructor(props) {
     super(props);
-    this._onClick = this._onClick.bind(this);
     this._onChange = this._onChange.bind(this);
-    // this.importState = this.importState.bind(this);
     this.state = {
       city: [],
       list: [],
@@ -51,92 +42,26 @@ export default class Weather extends React.Component {
 
   }
 
-  // componentDidMount() {
-  //   // let weatherURL = this.props.weatherURL;
-  //   // axios
-  //   //   .get(weatherURL)
-  //   //     .then(checkStatus)
-  //   //     .then(parseJSON)
-  //   //     .then(this.importState)
-  //   //     .catch(function(err) {
-  //   //       console.log(err);
-  //   //     });
-  // }
-
-  componentWillMount() {
-              console.log('component will  mount');
-      
-  console.log('this.state', this.state);
-
-  }
-
   componentDidMount() {
-            console.log('component did  mount');
-    // setTimeout(WeatherStore.getWeather(this._onChange), 4000);
-        // getForecast();
-        getForecast2();
-        WeatherStore.getWeather(this._onChange);
-        // console.log('WeatherStore.getWeather() did mount', WeatherStore.getWeather())
-        // let item = WeatherStore.getWeather();
-        // if (item.list.length !== 0) {
-        //   this.setState({list:item.list,
-        //                 city:item.city,
-        //           loading: item.loading
-        //               });
-        // }
-    // this.setState(WeatherStore.getWeather());
-
+    getForecast();
     WeatherStore.addChangeListener(this._onChange);
-
   }
 
   componentWillUnmount() {
-    console.log('component will un mount');
     WeatherStore.removeChangeListener(this._onChange);
   }
 
   _onChange() {
-    console.log('change event called');
-    console.log('WeatherStore.getWeather() on change', WeatherStore.getWeather());
-    // this.setState(WeatherStore.getWeather());
 
-       let item = WeatherStore.getWeather();
-        // if (item.list.length !== 0) {
-          this.setState({list:item.list,
-                        city:item.city,
-                  loading: item.loading
-                      });
-        // }
-    console.log('this.state onchange', this.state);
+    let item = WeatherStore.getWeather();
+    this.setState({
+      list:item.list,
+      city:item.city,
+      loading: item.loading
+    });
   }
-
-  _onClick(event) {
-    // console.log('clicked');
-        // console.log('WeatherStore.getWeather() on click', WeatherStore.getWeather());
-        // console.log('WeatherStore.getWeather() on click city', WeatherStore.getWeather().city);
-        // console.log('WeatherStore.getWeather() on click list', WeatherStore.getWeather().list);
-            // console.log('this.state clicked', this.state);
-        let item = WeatherStore.getWeather();
-        if (item.list.length !== 0) {
-          this.setState({list:item.list,
-                        city:item.city,
-                  loading: item.loading
-                      });
-        }
-        // getForecast();
-    // console.log('this.state.list.length', this.state.list.length)
-
-  }
-
-  // importState(item) {
-  //   this.setState({city:item.city});
-  //   this.setState({list:item.list});
-  // }
 
   render() {
-
-    console.log('this.state render', this.state)
-        console.log('this.state.list render', this.state.list)
 
     if(this.state.loading === false) {
           return (
@@ -145,7 +70,7 @@ export default class Weather extends React.Component {
 
         <h1 className="title">5 day weather forecast:  {this.state.city.name}, {this.state.city.country} </h1>
           {this.props.children}
-             
+
           <div className="weather-items">
 
            {this.state.list.map(function(listItem, i) {
@@ -154,17 +79,15 @@ export default class Weather extends React.Component {
 
                 <span>{listItem.dt_txt}</span>
 
-                   {
-                   listItem.weather.map(function(weatherItem) {
-                    var iconUrl = "http://openweathermap.org/img/w/" + weatherItem.icon + ".png";
+                  {listItem.weather.map(function(weatherItem) {
+                    let iconUrl = "http://openweathermap.org/img/w/" + weatherItem.icon + ".png";
                     return (
                       <ul style={ulStyle} key={weatherItem.id} className="item">
                         <li><img src={iconUrl} alt="alt"/></li>
                         <li>{weatherItem.description}</li>
                     </ul>
                     );
-                  })
-                }
+                  })}
 
                   <h3 className="temp">{
                   listItem.main.temp
@@ -176,7 +99,6 @@ export default class Weather extends React.Component {
             })}
 
           </div>
-
       </div>
 
     );
@@ -184,22 +106,29 @@ export default class Weather extends React.Component {
 
     else if (this.state.loading === true){
       return (
-        <div>loading...
-          <button
-          type="button"
-          onClick={this._onClick}
-          style={{
-              marginLeft: '.4rem',
-              borderRadius: '.4rem',
-              border: 'solid 1px #ccc',
-              padding: '0.4rem 1rem',
-              color: 'white',
-              fontSize: '1rem',
-              margin: '0 1rem',
-              marginTop: '13px',
-              cursor: 'pointer'}}>
-          click me
-        </button></div>
+        <div style={divStyle} >
+          <div>loading...</div>
+          <div className="loader loader--style8" >
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+               width="24px" height="30px" viewBox="0 0 24 30">
+              <rect x="0" y="10" width="4" height="10" fill="#333" opacity="0.2">
+                <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0s" dur="0.6s" repeatCount="indefinite" />
+                <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0s" dur="0.6s" repeatCount="indefinite" />
+                <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0s" dur="0.6s" repeatCount="indefinite" />
+              </rect>
+              <rect x="8" y="10" width="4" height="10" fill="#333"  opacity="0.2">
+                <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
+                <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
+                <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
+              </rect>
+              <rect x="16" y="10" width="4" height="10" fill="#333"  opacity="0.2">
+                <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
+                <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
+                <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
+              </rect>
+            </svg>
+          </div>
+        </div>
         )
     }
 
